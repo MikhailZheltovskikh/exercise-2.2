e494bc866fdce53bab5abd523a2bc3a08aad191d
MikhailZheltovskikh <misha14051994@mail.ru>
2024-04-02 00:04:37 +0500

    готовое решение задачи

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './app.module.css';
+import data from './data.json';
+import { useState } from 'react';
 
 export const App = () => {
+	const [steps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const lastItemList = activeIndex === steps.length - 1;
+
+	const onClickButtonNext = () => {
+		setActiveIndex(activeIndex + 1);
+	};
+
+	const onClickButtonPrev = () => {
+		if (activeIndex > 0) {
+			setActiveIndex(activeIndex - 1);
+		}
+	};
+
+	const onClickButtonReset = () => {
+		setActiveIndex(0);
+	};
+
+	const onClick = () => {
+		if (!lastItemList) {
+			onClickButtonNext();
+		} else {
+			onClickButtonReset();
+		}
+	};
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload .
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map(({ id, title }, index) => (
+							<li
+								className={
+									styles['steps-item'] +
+									(index === activeIndex ? ` ${styles.active}` : '') +
+									(index < activeIndex ? ` ${styles.done}` : '')
+								}
+								key={id}
+							>
+								<button className={styles['steps-item-button']}>
+									{index + 1}
+								</button>
+								{title}
+							</li>
+						))}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button className={styles.button} onClick={onClickButtonPrev}>
+							Назад
+						</button>
+						<button className={styles.button} onClick={onClick}>
+							{lastItemList ? 'Начать сначала' : 'Далее'}
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
